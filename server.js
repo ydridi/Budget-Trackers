c27c5dad17bce00b6f93a204021b9574945d1fe3
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-06-13 18:53:11 -0400

    rework on package.json

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds253408.mlab.com:53408/heroku_f5x3clrd";
+
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
