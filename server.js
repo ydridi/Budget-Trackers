fac08d62136a310b1b4eae7cf883c23d18935ecc
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-06-14 12:19:12 -0400

    servr update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
