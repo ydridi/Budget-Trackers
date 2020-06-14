ed7159fb1fd2bf9659cf3ee6334e5dc1bb0ef81c
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-06-14 11:59:43 -0400

    last update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
 
@@ -16,10 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds253408.mlab.com:53408/heroku_f5x3clrd";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
 
 // routes
 app.use(require("./routes/api.js"));
